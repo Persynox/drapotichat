fdbad4d83a185cd497964744f59e2fcd7fd61d33
Daephne <141509433+Persynox@users.noreply.github.com>
2023-08-05 20:51:43 +0200

    Update ServiceWorker.js

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -5,29 +5,33 @@ const contentToCache = [
     "Build/drapotichat.data",
     "Build/drapotichat.wasm",
     "TemplateData/style.css"
-
 ];
 
-self.addEventListener('install', function (e) {
-    console.log('[Service Worker] Install');
-    
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
-});
+// Vérifier si l'environnement est une extension Chrome
+const isChromeExtension = window.chrome && chrome.runtime && chrome.runtime.id;
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
+if (!isChromeExtension) {
+    self.addEventListener('install', function (e) {
+        console.log('[Service Worker] Install');
+        
+        e.waitUntil((async function () {
+            const cache = await caches.open(cacheName);
+            console.log('[Service Worker] Caching all: app shell and content');
+            await cache.addAll(contentToCache);
+        })());
+    });
 
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
+    self.addEventListener('fetch', function (e) {
+        e.respondWith((async function () {
+            let response = await caches.match(e.request);
+            console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+            if (response) { return response; }
+
+            response = await fetch(e.request);
+            const cache = await caches.open(cacheName);
+            console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+            cache.put(e.request, response.clone());
+            return response;
+        })());
+    });
+}
